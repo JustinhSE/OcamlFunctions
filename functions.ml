a67db31abbbd302d465dd8d0e8663cda31e9c405
Justin H <84724234+JustinhSE@users.noreply.github.com>
2024-04-14 12:23:29 -0400

    Update functions.ml

--- functions.ml
+++ functions.ml
@@ -1,1 +1,13 @@
+(* Every even natural number greater than 2 can be expressed as the sum of two prime numbers. It has been verified computationally for all integers less than 4 × 10^18. *) 
 
+let rec isPrime num a =
+if a < 2 then true 
+else if (num mod a) = 0 then false
+else isPrime num (a-1);;
+
+let goldbach_conjecture num =
+let rec helper x = 
+if num <= 2 then (-1,-1)
+else if isPrime (x) (x-1) == true && isPrime(num-x) (num-x-1) then (x, num-x)
+else helper (x+1)
+in helper 2;;
